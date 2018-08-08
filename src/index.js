9936d97f410ef155e40ec3f80730aef8e51a9396
谭森 <tansen@meikeland.com>
2018-08-08 10:36:08 +0800

    评论框基本结构

--- src/index.js
+++ src/index.js
@@ -1,8 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import CommentApp from "./CommentApp";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Title extends Component {
+  handleClickOnTitle(word, e) {
+    console.log(this, word);
+  }
+  render() {
+    return <h1 onClick={() => this.handleClickOnTitle("hello")}>React 小书</h1>;
+  }
+}
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <Title />
+        <h2>This is Header</h2>
+      </div>
+    );
+  }
+}
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This is main content</h2>
+      </div>
+    );
+  }
+}
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This is footer</h2>
+      </div>
+    );
+  }
+}
+
+class Index extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        {/* <Header />
+        <Main />
+        <Footer /> */}
+        <CommentApp />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Index />, document.getElementById("root"));
