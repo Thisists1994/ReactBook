2eb8e045c5498d4b7b7a9a46afbb4672a379de36
谭森 <tansen@meikeland.com>
2018-09-19 09:51:51 +0800

    创建store

--- make-react-redux/src/index.js
+++ make-react-redux/src/index.js
@@ -1,8 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+// import PropTypes from "prop-types";
+import ReactDOM from "react-dom";
+import Header from "./Header";
+import Content from "./Content";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+function createStore(reducer) {
+  let state = null;
+  const getState = () => state;
+  const dispatch = action => {
+    state = reducer(state, action);
+  };
+  return { getState, dispatch };
+}
+
+const themeReducer = (state, action) => {
+  if (!state) {
+    return {
+      themeColor: "red"
+    };
+  }
+  switch (action.type) {
+    case "CHANGE_COLOR":
+      return { ...state, themeColor: action.themeColor };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(themeReducer);
+store.dispatch({}); // 写action
+
+class Index extends Content {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Content />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Index />, document.getElementById("root"));
